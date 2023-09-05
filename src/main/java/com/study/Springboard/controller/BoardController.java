d1bb75900d604049cd09d2587c2e677cc43a8155
K1MTAEHONG <129834529+K1MTAEHONG@users.noreply.github.com>
2023-09-05 19:29:56 +0900

    modify

--- src/main/java/com/study/Springboard/controller/BoardController.java
+++ src/main/java/com/study/Springboard/controller/BoardController.java
@@ -19,6 +19,7 @@ public class BoardController {
     @GetMapping("/board/write")  //localhost:8000/board/write //어떤url로 접근할지 지정해주는 어노테이션
     public String boardWriteForm(){
 
+
         return "boardWrite"; // boardWrite 페이지를 보여주겠다
     }
 
